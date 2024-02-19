71347f372fde0b0c2bf37adf61a2b24ff7ff04c5
Vitrio <vitrio@noxus.co.id>
2024-02-19 07:43:05 +0700

    Added msal class

--- service/msal.ts
+++ service/msal.ts
@@ -1,3 +1,16 @@
+'use client'
+
 import * as msal from "@azure/msal-browser"
 
-const msalConfig = {}
+const msalConfig = {
+    auth: {
+        clientId: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID,
+        authority: `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID}`,
+        redirectUri: '/app-selector'
+    }
+}
+
+const msalInstance = new msal.PublicClientApplication(msalConfig)
+
+export { msalInstance }
+
